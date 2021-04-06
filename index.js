9f6ea4c0d97ae021f95299ae8bf77747c319a0a4
AmayChugh <2017.amay.chugh@ves.ac.in>
2021-04-06 21:20:08 +0530

    Deploy final

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const cors = require("cors");
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
 
-const io = require("socket.io")(server,{
+const PORT = process.env.PORT || 5000;
+
+const io = require('socket.io')(server, {
     cors: {
-        origin: "*",
-        methods: ["GET", "POST"]    
+        origin: '*',
+        methods: ['GET', 'POST']
     }
 });
 
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
-
 app.get("/", (req, res) => {
-    res.send("Server is running.");
+    res.send('Server is running on Port no 5000');
 });
 
-io.on('connection',(socket) => {
+io.on('connection', (socket) => {
     socket.emit('me', socket.id);
-    socket.on("disconnect",() =>{
-        socket.broadcast.emit("Call Ended");
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callEnded');
     });
-    socket.on('callUser',({userToCall, signalData, from, name}) =>{
+
+    socket.on('callUser', ({ userToCall, signalData, from, name }) => {
         io.to(userToCall).emit('callUser', { signal: signalData, from, name });
     });
-    socket.on('answercall', (data) => {
-        io.to(data.to).emit("Call Accepted", data.signal);
+
+    socket.on('answerCall', (data) => {
+        io.to(data.to).emit('callAccepted', data.signal);
     });
 });
+
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
